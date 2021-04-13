cee03f31e8f9b67b43761b16489ab96e6b398281
DmitryYatsyuk-dv <yatsyuk.dev@mail.ru>
2021-04-13 20:06:38 +0300

    Done:	UI-el. in the FeedCell & them layout; TODO:	Login page((UI-el)(log up/Sign up - a pages for registering new users and a page for already registered)).

--- CloneInstagram/View/FeedCell.swift
+++ CloneInstagram/View/FeedCell.swift
@@ -29,6 +29,58 @@ class FeedCell: UICollectionViewCell {
         return button
     }()
     
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.isUserInteractionEnabled = true
+        iv.image = #imageLiteral(resourceName: "flower")
+        return iv
+    }()
+    
+    private lazy var likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "like_unselected"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var commentButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "comment"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var shareButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "send2"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private let likesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1 like"
+        label.font = UIFont.boldSystemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Some test caption for now.."
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    private let postTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2 days ago"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .lightGray
+        return label
+    }()
+        
     //MARK: - Lifecycle
     
     override init(frame: CGRect) {
@@ -46,6 +98,26 @@ class FeedCell: UICollectionViewCell {
         usernameButton.centerY(inView: profileImageView,
                                leftAnchor: profileImageView.rightAnchor,
                                paddingLeft: 8)
+        
+        addSubview(postImageView)
+        postImageView.anchor(top: profileImageView.bottomAnchor,
+                             left: leftAnchor, right: rightAnchor,
+                             paddingTop: 8)
+        postImageView.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1).isActive = true
+        
+        configureActionButtons()
+        
+        addSubview(likesLabel)
+        likesLabel.anchor(top: likeButton.bottomAnchor, left: leftAnchor,
+                          paddingTop: -4, paddingLeft: 8)
+        
+        addSubview(captionLabel)
+        captionLabel.anchor(top: likesLabel.bottomAnchor, left: leftAnchor,
+                            paddingTop: 8, paddingLeft: 8)
+        
+        addSubview(postTimeLabel)
+        postTimeLabel.anchor(top: captionLabel.bottomAnchor, left: leftAnchor,
+                            paddingTop: 8, paddingLeft: 8)
     }
     
     required init?(coder: NSCoder) {
@@ -56,6 +128,21 @@ class FeedCell: UICollectionViewCell {
     @objc
     func didTapUsername() {
         print("DEBUG: did tap username")
+    }
+    
+    //MARK: - Helpers
+    
+    func configureActionButtons() {
+        let stackView = UIStackView(arrangedSubviews: [likeButton,
+                                                   commentButton,
+                                                   shareButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        
+        addSubview(stackView)
+        stackView.anchor(top: postImageView.bottomAnchor,
+                         width: 120, height: 50)
+        
     }
 }
 
